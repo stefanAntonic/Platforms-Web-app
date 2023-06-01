df3fb01b5b53a2749b22f37e34b3d6ef3e63227e
stefanAntonic <stefandantonic@gmail.com>
2023-06-01 20:34:42 +0200

    [#001]Adding CommandsService to monorepo

---
+++ CommandsService/Controllers/PlatformsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CommandsService.Controllers;
+
+[Route("api/controllers/[controller]")]
+[ApiController]
+public class PlatformsController : ControllerBase
+{
+    public PlatformsController()
+    {
+        
+    }
+
+    [HttpPost]
+    public IActionResult TestInboundConnection()
+    {
+        Console.WriteLine("--> Inbound POST # Comand Service");
+        return Ok("Inbound test of from Platforms Controller");
+    }
+}
