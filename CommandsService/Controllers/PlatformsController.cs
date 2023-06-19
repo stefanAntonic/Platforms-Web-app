c0770d425c241848ead0d280de1daf6f6f80cc61
stefanAntonic <stefandantonic@gmail.com>
2023-06-19 19:46:49 +0200

    [#001] Implementing ingress api gateway

--- CommandsService/Controllers/PlatformsController.cs
+++ CommandsService/Controllers/PlatformsController.cs
@@ -14,7 +14,7 @@ public class PlatformsController : ControllerBase
     [HttpPost]
     public IActionResult TestInboundConnection()
     {
-        Console.WriteLine("--> Inbound POST # Comand Service");
+        Console.WriteLine("--> Inbound POST # Command Service");
         return Ok("Inbound test of from Platforms Controller");
     }
 }
