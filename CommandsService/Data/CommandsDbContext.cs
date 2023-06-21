1ca3ef9b493305f548fa7c416c5ade082b29decf
stefanAntonic <stefandantonic@gmail.com>
2023-06-21 22:14:53 +0200

    [002] Created sql connection and deployment for platform service. Started elaboration of command service

---
+++ CommandsService/Data/CommandsDbContext.cs
@@ -0,0 +1,16 @@
+﻿using CommandsService.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CommandsService.Data;
+
+public class CommandsDbContext : DbContext
+{
+    public CommandsDbContext( DbContextOptions<CommandsDbContext> options): base(options)
+    {
+        
+    }
+
+    public DbSet<Command> Commands;
+    public DbSet<Platform> Platforms;
+
+}
---
+++ CommandsService/Models/Command.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CommandsService.Models;
+
+public class Command
+{
+    [Key]
+    [Required]
+    public int Id { get; set; }
+    [Required]
+    public string CommandName { get; set; }
+    [Required]
+    public string CommandLine { get; set; }
+    [Required]
+    public int PlatformId { get; set; }
+    public Platform Platform { get; set; }
+}
